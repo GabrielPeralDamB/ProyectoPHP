08e307e5de0f5c2f16a6b6ea5270684fd796d8a6
Gabriel Peral <gabriel.peral@iesdoctorbalmis.com>
2024-10-03 15:01:42 +0200

    primera version pagina

---
+++ mi-tienda-online/public/index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PAMBORGUINI</title>
+    <link rel="stylesheet" href="..\assets\css\styles.css">
+</head>
+
+<body>
+    <header>
+        <div class="logo">
+            <img src="../assets/images/logo.png" alt="PANBORGHINI">
+        </div>
+        <h1 class="main-title">PANBORGHINI</h1>
+        <nav>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Cars</a></li>
+                <li><a href="#">Dealership</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <section class="item">
+            <img src="../assets/images/bezoya.jpg">
+            <h2>BEZOYA</h2>
+                <p class="price">1€S</p>
+                <p class="description">One of the fastest supercars in the game with top speed and acceleration.</p>
+                <button class="buy-button">Buy Now</button>
+        </section>
+        <section class="item">
+            <img src="../assets/images/lanjaron.webp">
+            <h2>LANJARON</h2>
+            <p class="price">1,99€</p>
+            <p class="description">A sleek and stylish sports car that handles like a dream.</p>
+            <button class="buy-button">Buy Now</button>
+        </section>
+        <section class="item">
+            <img src="../assets/images/Nestle.jpg">
+            <h2>NESTLE</h2>
+            <p class="price">2,3€</p>
+            <p class="description">The king of speed, this car dominates every straight line in Los Santos.</p>
+            <button class="buy-button">Buy Now</button>
+        </section>
+
+    </main>
+
+    <footer>
+        <p>Legendary Motorsport © 2024. All rights reserved.</p>
+    </footer>
+</body>
+
+</html>
